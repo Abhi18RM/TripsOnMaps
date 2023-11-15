920d21b04466e368da5043996b7ba3871e21e760
Abhiram Ravuri <abhiramravuri@gmail.com>
2023-11-16 02:54:00 +0530

    wedding and cruises responsive done

--- src/components/Cruises/Cruises.jsx
+++ src/components/Cruises/Cruises.jsx
@@ -1,56 +1,124 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Cruises.css";
 
 const Cruises = () => {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const [windowSize, setWindowSize] = useState(getWindowSize());
 
-    return (
-        <div className="cruisesContainer" id="cruises">
-            <div className="cruisesTop">
-                <div className="topLeft">
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowSize(getWindowSize());
+        }
+
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+
+    function getWindowSize() {
+        const { innerWidth } = window;
+        return innerWidth > 650;
+    }
+
+    const FullResDes = () => {
+        return (
+            <div className="cruisesContainer" id="cruises">
+                <div className="cruisesTop">
+                    <div className="topLeft">
+                        <h1>Our Cruises</h1>
+                        <p>
+                            Make your Big Day innovative, luxurious and well
+                            arranged in our work passionated hands.
+                        </p>
+                        <div className="buttonContainer">
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="topCenter">
+                        <img src={PF + "/cruises/celebrity.png"} alt="" />
+                    </div>
+                    <div className="topRight">
+                        <img src={PF + "/cruises/princess.png"} alt="" />
+                    </div>
+                </div>
+                <div className="cruisesBottom">
+                    <div className="bottomLeft">
+                        <img
+                            className="shipImg"
+                            src={PF + "/cruises/bottomleft.png"}
+                            alt=""
+                        />
+                        <div className="bLTopL">
+                            <img src={PF + "/cruises/norwegian.png"} alt="" />
+                        </div>
+                        <div className="bLBottomR">
+                            <img src={PF + "/cruises/royal.png"} alt="" />
+                        </div>
+                    </div>
+                    <div className="bottomRight">
+                        <img
+                            src={PF + "/cruises/bottomright.png"}
+                            alt=""
+                            className="shipImg"
+                        />
+                        <div className="bRTopL">
+                            <img src={PF + "/cruises/holland.png"} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    const ResDes = () => {
+        return (
+            <div className="resCruCon">
+                <div className="top">
                     <h1>Our Cruises</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Temporibus fugiat
+                    <p style={{ textAlign: "center" }}>
+                        Make your Big Day innovative, luxurious and well
+                        arranged in our work passionated hands.
                     </p>
-                    <div className="buttonContainer">
+                    <div
+                        className="buttonContainer"
+                        style={{ justifyContent: "center" }}
+                    >
                         <button>See More</button>
                     </div>
                 </div>
-                <div className="topCenter">
-                    <img src={PF + "/cruises/celebrity.png"} alt="" />
-                </div>
-                <div className="topRight">
-                    <img src={PF + "/cruises/princess.png"} alt="" />
-                </div>
-            </div>
-            <div className="cruisesBottom">
-                <div className="bottomLeft">
-                    <img
-                        className="shipImg"
-                        src={PF + "/cruises/bottomleft.png"}
-                        alt=""
-                    />
-                    <div className="bLTopL">
+                <div className="cruiseImages">
+                    <div
+                        className="cruiseImage"
+                        style={{ backgroundColor: "#E0F7FF" }}
+                    >
+                        <img src={PF + "/cruises/princess.png"} alt="" />
+                    </div>
+                    <div className="cruiseImage">
+                        <img src={PF + "/cruises/celebrity.png"} alt="" />
+                    </div>
+                    <div
+                        className="cruiseImage"
+                        style={{ backgroundColor: "#E0F7FF" }}
+                    >
                         <img src={PF + "/cruises/norwegian.png"} alt="" />
                     </div>
-                    <div className="bLBottomR">
+                    <div className="cruiseImage">
                         <img src={PF + "/cruises/royal.png"} alt="" />
                     </div>
-                </div>
-                <div className="bottomRight">
-                    <img
-                        src={PF + "/cruises/bottomright.png"}
-                        alt=""
-                        className="shipImg"
-                    />
-                    <div className="bRTopL">
+                    <div
+                        className="cruiseImage"
+                        style={{ backgroundColor: "#090E2C" }}
+                    >
                         <img src={PF + "/cruises/holland.png"} alt="" />
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    };
+
+    return <>{windowSize ? <FullResDes /> : <ResDes />}</>;
 };
 
 export default Cruises;
--- src/components/Wedding/Wedding.jsx
+++ src/components/Wedding/Wedding.jsx
@@ -18,22 +18,53 @@ const Wedding = () => {
     }, []);
 
     function getWindowSize() {
-        const { innerWidth, innerHeight } = window;
-        return innerWidth;
+        const { innerWidth } = window;
+        return innerWidth > 750;
     }
 
-    return (
-        <div className="weddingContainer" id="wedding">
-            <div className="weddingLeft">
-                <div className="weddingImage">
-                    <img src={PF + `/weddings/italy.png`} alt="" />
+    const FullSizeDesign = () => {
+        return (
+            <div className="weddingContainer" id="wedding">
+                <div className="weddingLeft">
+                    <div className="weddingImage">
+                        <img src={PF + `/weddings/italy.png`} alt="" />
+                    </div>
+                    <div className="leftInfo">
+                        <h3>The Amalfi Coast, Italy</h3>
+                    </div>
                 </div>
-                <div className="leftInfo">
-                    <h3>The Amalfi Coast, Italy</h3>
+                <div className="weddingCenter">
+                    <div className="weddingInfo">
+                        <h1>Destination Weddings</h1>
+                        <p>
+                            Make your Big Day innovative, luxurious and well
+                            arranged in our work passionated hands.
+                        </p>
+                        <button>See More</button>
+                    </div>
+                    <div className="weddingImage">
+                        <img src={PF + "/weddings/greece.png"} alt="" />
+                    </div>
+                    <div className="centerInfo">
+                        <h3>Santorini, Greece</h3>
+                    </div>
+                </div>
+                <div className="weddingRight">
+                    <div className="weddingImage">
+                        <img src={PF + "/weddings/spain.png"} alt="" />
+                    </div>
+                    <div className="rightInfo">
+                        <h3>Marbella, Spain</h3>
+                    </div>
                 </div>
             </div>
-            <div className="weddingCenter">
-                <div className="weddingInfo">
+        );
+    };
+
+    const ResponsiveDesign = () => {
+        return (
+            <div className="weddingConRes">
+                <div className="weddingInfoRes">
                     <h1>Destination Weddings</h1>
                     <p>
                         Make your Big Day innovative, luxurious and well
@@ -41,23 +72,40 @@ const Wedding = () => {
                     </p>
                     <button>See More</button>
                 </div>
-                <div className="weddingImage">
+                <div className="weddingImg">
+                    <img src={PF + `/weddings/italy.png`} alt="" />
+                    <div className="topInfo">
+                        <h3>The Amalfi Coast, Italy</h3>
+                    </div>
+                </div>
+                <div
+                    className="weddingImg"
+                    style={{
+                        justifyContent: "flex-end",
+                        transform: "translateY(-50px)",
+                    }}
+                >
                     <img src={PF + "/weddings/greece.png"} alt="" />
+                    <div className="middleInfo">
+                        <h3>Santorini, Greece</h3>
+                    </div>
                 </div>
-                <div className="centerInfo">
-                    <h3>Santorini, Greece</h3>
-                </div>
-            </div>
-            <div className="weddingRight">
-                <div className="weddingImage">
+                <div
+                    className="weddingImg"
+                    style={{
+                        transform: "translateY(-100px)",
+                    }}
+                >
                     <img src={PF + "/weddings/spain.png"} alt="" />
-                </div>
-                <div className="rightInfo">
-                    <h3>Marbella, Spain</h3>
+                    <div className="bottomInfo">
+                        <h3>Marbella, Spain</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    };
+
+    return <>{windowSize ? <FullSizeDesign /> : <ResponsiveDesign />}</>;
 };
 
 export default Wedding;
